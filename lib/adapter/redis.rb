30ebfb48b792b24d26f574f2ffee139da578a354
Jake Howerton <jake.howerton@gmail.com>
2011-04-05 00:30:25 -0400

    woops fixing reversed conditionals and delete

--- lib/adapter/redis.rb
+++ lib/adapter/redis.rb
@@ -5,22 +5,28 @@ module Adapter
   module Redis
     def read(key)
       if @options[:hash]
-        decode(client.get(key_for(key)))
-      else
         decode(client.hget(@options[:hash], key_for(key)))
+      else
+        decode(client.get(key_for(key)))
       end
     end
 
     def write(key, value)
       if @options[:hash]
-        client.set(key_for(key), encode(value))
+        client.hset(@options[:hash],key_for(key), encode(value))
       else
-        client.hset(@options[:hash],key_for(key), encode(value))    
+        client.set(key_for(key), encode(value))    
       end
     end
 
     def delete(key)
-      read(key).tap { client.del(key_for(key)) }
+      read(key).tap do
+        if @options[:hash]
+          client.hdel(@options[:hash],key_for(key))
+        else
+          client.del(key_for(key))
+        end
+      end
     end
 
     def clear
